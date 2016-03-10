ba39244c7d8c79fa0294f784845ba2fbfc058f96
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-10 19:47:51 +0530

    Persisting Preferences, reading, removing from mongodb

--- sync_preference.py
+++ sync_preference.py
@@ -1,19 +1,28 @@
-from bottle import route, run, get, delete, post, request
+from bottle import route, run, get, delete, post, request, abort
+from pymongo import MongoClient
+import json
+import pdb
+
+client = MongoClient('localhost', 27017)
+preference_collection = client.preference_db.preferences
+
 
 @post('/users/<user_id:int>/preferences.json')
 def add_preference(user_id):
-    preferences = request.body.read()
-    print(preferences);
-    print("adding preferences for user")
-    return "success" + str(user_id)
+    pref_body = request.body.read().decode("utf-8")
+    user_preference = json.loads(pref_body)
+    inserted_record = preference_collection.insert_one({'user_id': user_id, 'preference': user_preference})
+    return str(inserted_record.inserted_id)
 
 @delete('/users/<user_id:int>/preferences/destroy')
 def delete_preference(user_id):
-    print("deletes...")
+    return str(preference_collection.delete_one({ 'user_id': user_id }).deleted_count)
 
 @get('/users/<user_id:int>/preferences.json')
 def get_preference(user_id):
-    print("It hits...")
-    return "Success" + str(user_id)
+    user_pref = preference_collection.find_one({'user_id': user_id})
+    if user_pref:
+        return user_pref['preference']
+    else: abort(404, "Preference Not Found")
 
 run(host='localhost', port=8888, debug=True, reloader=True)
