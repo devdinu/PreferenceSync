5104a53d8e99e39d65311732dded7a9520f54287
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-20 22:53:19 +0530

    Updating record, sync files

--- sync_preference_service.py
+++ sync_preference_service.py
@@ -5,17 +5,30 @@ import pdb
 
 client = MongoClient('localhost', 27017)
 preference_collection = client.preference_db.preferences
+DOT_CHAR = "."
+DOT_UNICODE = "\\uff0"
 
 
 @post('/users/<user_id>/preferences.json')
 def add_preference(user_id):
     pref_body = request.body.read().decode("utf-8")
-    print(pref_body)
     user_preference = json.loads(pref_body)
     print("storing information for user_id:" + user_id)
-    inserted_record = preference_collection.replace_one(
-        {'user_id': user_id}, {'user_id': user_id, 'preference': user_preference}, upsert=True)
-    return str(inserted_record.upserted_id)
+    data_to_update = {'user_id': user_id,
+                      'preference': user_preference}
+    upserted_record = preference_collection.update_one(
+        {'user_id': user_id}, {"$set": data_to_update}, upsert=True)
+    return str(upserted_record.upserted_id)
+
+
+@post('/users/<user_id>/files/<file_name>/sync.json')
+def add_file(user_id, file_name):
+    file_content = request.body.read().decode("utf-8")
+    file_name = file_name.replace(DOT_CHAR, DOT_UNICODE)
+    data_to_store = {'user_id': user_id, 'file_name': file_name, 'content': file_content}
+    upserted_file = preference_collection.replace_one(
+        {'user_id': user_id, 'file_name': file_name}, data_to_store, upsert=True)
+    return str(upserted_file.upserted_id)
 
 
 @delete('/users/<user_id>/preferences/destroy')
