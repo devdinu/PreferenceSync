90ec27900f4bc4a35208fd7a1c1c7a201656de89
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-20 22:54:44 +0530

    sync file contents to server

--- plugin/sync_api.py
+++ plugin/sync_api.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 
@@ -7,3 +8,8 @@ class SyncApi:
         print("saving for {0} with {1}".format(id, files))
         payload = {'files_to_sync': files}
         requests.post(self.host + "users/{0}/preferences.json".format(id), data=json.dumps(payload))
+
+    def sync_file(self, id, dir_name, file_name):
+        print("syncing file" + file_name)
+        file_content = open(os.path.join(dir_name, file_name)).read()
+        requests.post(self.host + "users/{0}/files/{1}/sync.json".format(id, file_name), data=file_content)
--- plugin/sync_preference.py
+++ plugin/sync_preference.py
@@ -16,19 +16,22 @@ class SyncPreferencesCommand(sublime_plugin.TextCommand):
         files_to_sync = self.get_files_to_sync()
         user_id = self._get_user_id()
         self.sync_api.sync_file_names(user_id, files_to_sync)
+        dir_name = self.settings.get(USER_PACKAGE_LOCATION)
+        for file in files_to_sync:
+            self.sync_api.sync_file(user_id, dir_name, file)
 
     def get_files_to_sync(self):
-        package_location = self.settings.get(USER_PACKAGE_LOCATION)
-        exclude_files = self.settings.get(FILES_TO_EXCLUDE)
-        files_to_sync = [file for file in os.listdir(package_location) if os.path.isfile(
+        package_location=self.settings.get(USER_PACKAGE_LOCATION)
+        exclude_files=self.settings.get(FILES_TO_EXCLUDE)
+        files_to_sync=[file for file in os.listdir(package_location) if os.path.isfile(
             os.path.join(package_location, file)) and file not in exclude_files]
         return files_to_sync
 
     def _get_user_id(self):
-        user_id = self.settings.get(USER_ID)
+        user_id=self.settings.get(USER_ID)
         if not user_id:
             print("Generating user_id for first time")
-            user_id = uuid.uuid4()
+            user_id=uuid.uuid4()
             self._save_id(user_id)
         return user_id
 
