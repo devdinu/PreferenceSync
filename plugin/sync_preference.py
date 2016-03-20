8b86a2e17dcc50aa61d450382d86540ed807c7df
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-20 19:07:26 +0530

    Fixing typos and making plugin work, adding trigger key

--- plugin/sync_preference.py
+++ plugin/sync_preference.py
@@ -1,24 +1,24 @@
 import os
 import uuid
-
+import pdb
 import sublime
 import sublime_plugin
 
-from config import *
+from .configs import *
 
-class SyncPreferenceCommand(sublime_plugin.TextCommand):
+class SyncPreferencesCommand(sublime_plugin.TextCommand):
 
     settings = sublime.load_settings(SYNC_PREFERENCES_FILE)
 
     def sync_files(self):
         files_to_sync = self.get_files_to_sync()
         unique_id = uuid.uuid4()
-        save_unique_id(unique_id)
+        self.save_id(unique_id)
         print("saving for {0} with {1}".format(unique_id, files_to_sync))
 
     def get_files_to_sync(self):
-        package_location = settings.get(USER_PACKAGE_LOCATION)
-        exclude_files = settings.get(FILES_TO_EXCLUDE)
+        package_location = self.settings.get(USER_PACKAGE_LOCATION)
+        exclude_files = self.settings.get(FILES_TO_EXCLUDE)
         files_to_sync = [ file
             for file in os.listdir(package_location)
             if os.path.isfile(os.path.join(package_location, file))
@@ -26,8 +26,8 @@ class SyncPreferenceCommand(sublime_plugin.TextCommand):
             ]
         return files_to_sync
 
-    def save_id(id):
-        settings.set("unique_id", id)
+    def save_id(self, id):
+        self.settings.set("unique_id", str(id))
 
     def run(self, edit):
         self.sync_files()
