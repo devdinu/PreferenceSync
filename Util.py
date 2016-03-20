d8225d4298f55885a34e2231852ce221d7d1102d
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-21 01:26:04 +0530

    Api hitting service to get list of files, and getting file content for a given file

---
+++ Util.py
@@ -0,0 +1,14 @@
+import os
+import sublime
+from .configs import SYNC_PREFERENCES_FILE, USER_PACKAGE_LOCATION
+
+
+class Util:
+
+    @staticmethod
+    def get_user_package_location():
+        settings = sublime.load_settings(SYNC_PREFERENCES_FILE)
+        location = settings.get(USER_PACKAGE_LOCATION)
+        substituted_location = [folder if "$" not in folder
+                                else os.environ[folder[1:]] for folder in location.split("/")]
+        return "/".join(substituted_location)
--- sync_api.py
+++ sync_api.py
@@ -2,14 +2,30 @@ import os
 import requests
 import json
 
+
 class SyncApi:
     host = "http://localhost:8888/"
+
     def sync_file_names(self, id, files):
         print("saving for {0} with {1}".format(id, files))
         payload = {'files_to_sync': files}
         requests.post(self.host + "users/{0}/preferences.json".format(id), data=json.dumps(payload))
 
     def sync_file(self, id, dir_name, file_name):
-        print("syncing file" + file_name)
+        print("syncing file: " + file_name)
         file_content = open(os.path.join(dir_name, file_name)).read()
-        requests.post(self.host + "users/{0}/files/{1}/sync.json".format(id, file_name), data=file_content)
+        requests.post(
+            self.host + "users/{0}/files/{1}/sync.json".format(id, file_name), data=file_content)
+
+    def get_files_to_download(self, id):
+        preference = requests.get(self.host + "users/{0}/preferences.json".format(id))
+        if preference.status_code == 200:
+            return preference.json()['files_to_sync']
+        return None
+
+    def get_file_contents(self, id, file_name):
+        response = requests.get(
+            self.host + "users/{0}/files/{1}/content.json".format(id, file_name))
+        if response.status_code == 200:
+            return response.text
+        return None
