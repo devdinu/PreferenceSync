0b2619c1d168aa64fd9811fe319f3d22a7899f37
Dinesh Kumar <dineshkb@thoughtworks.com>
2016-03-21 19:57:35 +0530

    Using util class method to get package name, changing trigger key for sync

--- sync_preference.py
+++ sync_preference.py
@@ -1,14 +1,17 @@
 import os
 import uuid
+import shutil
+from uuid import UUID
+import datetime
+
 import sublime
 import sublime_plugin
-
+from .util import Util
 from .sync_api import SyncApi
 from .configs import *
 
 
 class SyncPreferencesCommand(sublime_plugin.TextCommand):
-
     settings = sublime.load_settings(SYNC_PREFERENCES_FILE)
     sync_api = SyncApi()
 
@@ -16,12 +19,12 @@ class SyncPreferencesCommand(sublime_plugin.TextCommand):
         files_to_sync = self.get_files_to_sync()
         user_id = self._get_user_id()
         self.sync_api.sync_file_names(user_id, files_to_sync)
-        dir_name = self._get_user_package_location()
+        dir_name = Util.get_user_package_location()
         for file in files_to_sync:
             self.sync_api.sync_file(user_id, dir_name, file)
 
     def get_files_to_sync(self):
-        package_location = self._get_user_package_location()
+        package_location = Util.get_user_package_location()
         exclude_files = self.settings.get(FILES_TO_EXCLUDE)
         files_to_sync = [file for file in os.listdir(package_location) if os.path.isfile(
             os.path.join(package_location, file)) and file not in exclude_files]
@@ -37,12 +40,6 @@ class SyncPreferencesCommand(sublime_plugin.TextCommand):
 
     def _save_id(self, id):
         self.settings.set(USER_ID, str(id))
-
-    def _get_user_package_location(self):
-        location = self.settings.get(USER_PACKAGE_LOCATION)
-        substituted_location = [folder if "$" not in folder
-                                else os.environ[folder[1:]] for folder in location.split("/")]
-        return "/".join(substituted_location)
 
     def run(self, edit):
         self.sync_files()
